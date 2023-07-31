a5b6535497faccdcd1d1e48f972374b585f0a01a
omkeshborse <bomkes25@gmail.com>
2023-07-31 09:11:18 +0530

    CORS configure

--- seoblog/backend/server.js
+++ seoblog/backend/server.js
@@ -1,30 +1,31 @@
-require('dotenv').config() ;
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const cors = require("cors"); 
+const cors = require("cors");
 
-
-const app = express() ;
+const app = express();
 
 /* middle ware */
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(express.json());
-app.use(cookieParser()) ;
+app.use(cookieParser());
 
 /* cors */
-app.use(cors())
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 /* routes */
-app.get('/api' , (req, res)=>{
-    res.json({
-        time :Date().toString() 
-    })
-})
+app.get("/api", (req, res) => {
+  res.json({
+    time: Date().toString(),
+  });
+});
 
-const PORT = process.env.PORT || 8080 ;
-app.listen(PORT , () =>{
-    console.log(`server is running at http://localhost:${PORT}`);
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
